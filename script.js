bef8e753372e1370ed6f9570199462f3ddcdcc7e
M-lie <camelielaprise@hotmail.com>
2023-09-29 17:45:01 -0400

    PO-avancement-accueil-3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".mySwiper", {
+const swiperHero = new Swiper(".mySwiper", {
     direction: "horizontal",
     slidesPerView: 1,
     mousewheel: true,
@@ -6,4 +6,15 @@ const swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
-  });
+});
+
+const swiperNews = new Swiper(".mySecondSwiper", {
+  direction: "horizontal",
+  slidesPerView: 2,
+  spaceBetween: 15,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
