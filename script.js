520c70219b56fc3ca594518b06851eb8eff2e833
M-lie <camelielaprise@hotmail.com>
2023-10-07 14:38:43 -0400

    PO-accueil-avancement-7

--- script.js
+++ script.js
@@ -39,3 +39,29 @@ hamburger.addEventListener('mouseenter', () => {
     },
   });
 });
+
+/*const sentencesArr = ["Offrez votre charité", "Faites un don"];
+
+let cursor = gsap.to(".cursor", {opacity: 0, ease: "power2.inOut", repeat: -1});
+
+gsap.from(".contenu svg", {x: "15%", yoyo: true, ease: "power1.inOut", repeat: -1});
+
+let animation = gsap.timeline({repeat: -1});
+
+sentencesArr.forEach(sentence => {
+  let timeLine = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 1});
+  timeLine.to(".text", {duration: 1.5, text: sentence});
+  animation.add(timeLine);
+});*/
+
+const sentence = "Faites un don";
+
+let cursor = gsap.to(".cursor", {opacity: 0, ease: "power2.inOut", repeat: -1});
+
+gsap.from(".contenu svg", {x: "15%", yoyo: true, ease: "power1.inOut", repeat: -1});
+
+let animation = gsap.timeline({repeat: -1});
+
+let timeLine = gsap.timeline();
+timeLine.to(".text", {duration: 2, text: sentence});
+animation.add(timeLine);
