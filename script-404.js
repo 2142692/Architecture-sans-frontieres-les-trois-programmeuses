a714aec263b259acfd0a3ccc69f8973cbf20f316
M-lie <camelielaprise@hotmail.com>
2023-10-25 14:18:39 -0400

    correction-burger-animation-erreur-404

--- script-404.js
+++ script-404.js
@@ -1,3 +1,24 @@
+// --- DÉBUT Hambuger Animation ---
+const hamburger = document.querySelector('.hamburger');
+const lineOne = hamburger.querySelector('.upper');
+const lineTwo = hamburger.querySelector('.middle');
+const lineThree = hamburger.querySelector('.lower');
+
+const lines = [lineOne, lineTwo, lineThree];
+
+hamburger.addEventListener('mouseenter', () => {
+  gsap.timeline().to(lines, { duration: 0.2, ease: 'power2.inOut',
+    scaleX: 1.5,
+    stagger: {
+      each: 0.125,
+      repeat: 1,
+      yoyo: true,
+    },
+  });
+});
+// --- FIN Hamburger Animation ---
+
+// --- DÉBUT Erreur 404 Animation ---
 let anim = gsap
     .timeline({}) 
     .to(".clip-top .marquee, .clip-bottom .marquee", 5, {
@@ -13,5 +34,4 @@ let anim = gsap
         yoyo: true,
         repeat: -1,
     });
-
-
+// --- FIN Erreur 404 Animation ---
