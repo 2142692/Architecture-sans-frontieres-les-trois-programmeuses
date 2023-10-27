b0a4725fc8c1294b027db27937614a42a994a736
2142692 <2142692@cmontmorency.qc.ca>
2023-10-27 17:52:35 -0400

    ligne-du-temps-amelioration

--- script-ligne.js
+++ script-ligne.js
@@ -1,9 +1,9 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const mask = document.querySelector('#Rectangle_1_3');
-const sections = gsap.utils.toArray('.histoire_desc');
+const mask = document.querySelector('#groupe');
+
 const section = document.querySelector('.histoire_desc');
-const svg = document.querySelector('svg');
+
 /*let scrollOne = gsap.to(sections, {
     yPercent: -100 * (sections.length - 1),
     ease: "none",
@@ -13,17 +13,15 @@ const svg = document.querySelector('svg');
         scrub: 1,
         end: "+=3000"
     }
-})
-*/
+})*/
+
 gsap.to(mask, {
-    height: "90%",
+    height: "100%",
     scrollTrigger:{
         trigger: section,
         markers: "true",
-        
-        start: "9% 20%",
-        
+        start: "9% 24%",
+        end: "50%",
         scrub:1,
-        
     }
 })
