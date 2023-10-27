6f78ab4052fbde5c59abed310cb358e7b0dc5bbd
2142692 <2142692@cmontmorency.qc.ca>
2023-10-27 11:14:14 -0400

    devop_animation_ligne_success

--- script-ligne.js
+++ script-ligne.js
@@ -1,10 +1,29 @@
+gsap.registerPlugin(ScrollTrigger);
+
 const mask = document.querySelector('#Rectangle_1_3');
 const sections = gsap.utils.toArray('.histoire_desc');
-
-gsap.to(mask, {
-    width: "100%",
+const section = document.querySelector('.histoire_desc');
+const svg = document.querySelector('svg');
+/*let scrollOne = gsap.to(sections, {
+    yPercent: -100 * (sections.length - 1),
+    ease: "none",
     scrollTrigger:{
-        trigger: "sections",
-        start: "top bottom"
+        trigger: sections,
+        pin: true,
+        scrub: 1,
+        end: "+=3000"
+    }
+})
+*/
+gsap.to(mask, {
+    height: "90%",
+    scrollTrigger:{
+        trigger: section,
+        markers: "true",
+        
+        start: "9% 20%",
+        
+        scrub:1,
+        
     }
 })
