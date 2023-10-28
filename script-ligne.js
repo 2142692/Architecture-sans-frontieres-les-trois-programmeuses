5c016bde5470fe57ff5cd6e92f22f76bcb482a59
M-lie <camelielaprise@hotmail.com>
2023-10-28 00:35:27 -0400

    nouvelles-arrangements-banniere-hero

--- script-ligne.js
+++ script-ligne.js
@@ -1,5 +1,25 @@
 gsap.registerPlugin(ScrollTrigger);
 
+// --- DÉBUT Hambuger Animation ---
+const hamburger = document.querySelector('.hamburger');
+const lineOne = hamburger.querySelector('.upper');
+const lineTwo = hamburger.querySelector('.middle');
+const lineThree = hamburger.querySelector('.lower');
+
+const lines = [lineOne, lineTwo, lineThree];
+
+hamburger.addEventListener('mouseenter', () => {
+  gsap.timeline().to(lines, { duration: 0.2, ease: 'power2.inOut',
+    scaleX: 1.5,
+    stagger: {
+      each: 0.125,
+      repeat: 1,
+      yoyo: true,
+    },
+  });
+});
+// --- FIN Hamburger Animation ---
+
 const mask = document.querySelector('#groupe');
 
 const section = document.querySelector('.histoire_desc');
