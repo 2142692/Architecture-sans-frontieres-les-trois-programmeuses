56989b3a52664b54a8e7866b5ca1944f66680653
M-lie <camelielaprise@hotmail.com>
2023-11-03 14:55:19 -0400

    equipe-page-menu

--- script.js
+++ script.js
@@ -60,31 +60,4 @@ timeLine.to(".text", {duration: 2, text: sentence});
 animation.add(timeLine);
 // --- FIN Don Animation ---
 
-// --- DÉBUT Don Animation EN ---
-const sentenceEn = "Make a donation";
-
-let cursorEn = gsap.to(".cursor-en", {opacity: 0, ease: "power2.inOut", repeat: -1});
-
-gsap.from(".contenu-en svg", {x: "15%", yoyo: true, ease: "power1.inOut", repeat: -1});
-
-let animationEn = gsap.timeline({repeat: -1});
-
-let timeLineEn = gsap.timeline();
-timeLineEn.to(".text-en", {duration: 2, text: sentenceEn});
-animationEn.add(timeLineEn);
-// --- FIN Don Animation EN ---
-
-// --- DÉBUT Erreur 404 Animation ---
-gsap.registerPlugin(ScrollTrigger);
-let erreur = gsap.timeline()
-.from(".erreur_titre",
-{opacity: 0, repeat: -2, ease: "power2.inOut", duration:0.7, yoyo:true})
-
-.from(".erreur_desc",
-{opacity: 0, y: 50, duration:3})
-
-
-
-
-// --- FIN Erreur 404 Animation---
 
